526179592db32f925c32ee4cd6ba41c202a793c2
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-02-19 11:15:53 -0500

    Add files via upload

---
+++ S2-3.py
@@ -0,0 +1,25 @@
+s= input()
+code=[]
+patternl = 0
+letter= int(input())+1
+y=1
+for x in range(len(s)):
+    if s[x].isalpha():
+      code.append(s[x])
+      s = s.replace(s[x], " ") 
+
+numA= s.split()
+for y in range(len(numA)):
+  patternl = patternl + int(numA[y])
+remainder= letter%patternl
+if remainder==0:
+   let= code[-1]
+else:
+    for z in range(len(numA)):
+        remainder = remainder- int(numA[z])
+        if remainder <=0:
+            let= code[z]
+            break
+print(let)
+   
+
